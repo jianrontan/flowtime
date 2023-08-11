d9093d29b063741abf0ce18d5d81e7d18ce011e6
jianrontan <116509027+jianrontan@users.noreply.github.com>
2023-08-11 13:45:22 +0800

    Bar chart in statistics encounters error

--- screens/statistics.js
+++ screens/statistics.js
@@ -1,6 +1,7 @@
 import { useState, useEffect, useCallback } from 'react';
 import { View, ScrollView, SafeAreaView, TouchableOpacity, StyleSheet, Text } from 'react-native';
 import CalendarPicker from  'react-native-calendar-picker';
+import { StackedBarChart } from 'react-native-chart-kit';
 import { db } from '../config/firebase';
 import { collection, query, where, getDocs, Timestamp, onSnapshot } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
@@ -15,6 +16,19 @@ function Statistics() {
   const maxDate = new Date();
   const [groupsRange, setGroupsRange] = useState({});
   const [groupsTotal, setGroupsTotal] = useState({});
+  const [selectedMonday, setSelectedMonday] = useState(new Date());
+  const [weeklyData, setWeeklyData] = useState([]);
+
+  // SET selectedMonday TO BE MOST RECENT MONDAY //
+  useEffect(() => {
+    const today = new Date();
+    const day = today.getDay();
+    const diff = today.getDate() - day + (day === 0 ? -6 : 1);
+    const lastMonday = new Date(today.setDate(diff));
+    
+    setSelectedMonday(lastMonday);
+  }, []);
+  // SET selectedMonday TO BE MOST RECENT MONDAY //
 
   // FUNCTION TO SELECT AND STORE SELECTED DATES //
   function onDateChange(date, type) {
@@ -122,6 +136,83 @@ function Statistics() {
   }, []);
   // FETCH DATA WHENEVER USER SELECTS NEW DATE //
 
+  // HANDLE RANGE FOR BAR CHART //
+  function onPreviousPress() {
+    const currentMonday = new Date(selectedMonday);
+    currentMonday.setDate(currentMonday.getDate() - 7);
+    setSelectedMonday(currentMonday);
+  }
+
+  function onNextPress() {
+    const currentMonday = new Date(selectedMonday);
+    currentMonday.setDate(currentMonday.getDate() + 7);
+    const today = new Date();
+    today.setHours(0, 0, 0, 0);
+    if (currentMonday <= today) {
+      setSelectedMonday(currentMonday);
+    }
+  }
+  // HANDLE RANGE FOR BAR CHART //
+
+  // DATA FOR BAR CHART //
+  async function fetchChartData() {
+    const weekStart = startOfDay(new Date(selectedMonday));
+    const weekEnd = endOfDay(new Date(selectedMonday));
+    weekEnd.setDate(weekEnd.getDate() + 6); // Add six days to get the end of the week
+  
+    const weekStartTimestamp = Timestamp.fromDate(weekStart);
+    const weekEndTimestamp = Timestamp.fromDate(weekEnd);
+  
+    const q = query(collection(db, "statistics"),
+      where("id", "==", userId),
+      where("ms", ">=", weekStartTimestamp),
+      where("ms", "<=", weekEndTimestamp));
+  
+    const weeklyData = [0, 0, 0, 0, 0, 0, 0]; // Represents the 7 days of the week
+    const daysOfWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+    const weeklyGroups = {};
+  
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      const data = doc.data();
+      const time = toSeconds(data.length);
+      const tag = data.tagged;
+      const date = new Date(data.ms * 1000);
+      const dayOfWeek = daysOfWeek[date.getDay()];
+  
+      if (!weeklyGroups[dayOfWeek]) {
+        weeklyGroups[dayOfWeek] = {[tag]: time}; 
+      } else if (!weeklyGroups[dayOfWeek][tag]) {
+        weeklyGroups[dayOfWeek][tag] = time;
+      } else {
+        weeklyGroups[dayOfWeek][tag] += time;   
+      }
+    });
+  
+    for (let i = 0; i < weeklyData.length; i++) {
+      if (weeklyGroups[daysOfWeek[i]]) {
+        weeklyData[i] = weeklyGroups[daysOfWeek[i]]; 
+      }
+    }
+  
+    setWeeklyData(weeklyData);
+  }
+  // DATA FOR BAR CHART //
+
+  // FETCH CHART DATA EVERY TIME USER CHANGED WEEK //
+  useEffect(() => {
+    fetchChartData();
+  }, [selectedMonday]);
+  // FETCH CHART DATA EVERY TIME USER CHANGED WEEK //
+
+  const labels = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+  const data = {
+    labels: labels,
+    legend: ['L1', 'L2', 'L3'], // Adjust this based on your tags
+    data: weeklyData, // Your data
+    barColors: ['#dfe4ea', '#ced6e0', '#a4b0be']
+  };
+
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightBeige }}>
       <ScrollView showsVerticalScrollIndicator={false}>
@@ -219,6 +310,38 @@ function Statistics() {
             })}
           </View>
         </View>
+
+        <View>
+          <TouchableOpacity onPress={onPreviousPress}>
+            <Text>Previous</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity onPress={onNextPress}>
+            <Text>Next</Text>
+          </TouchableOpacity>
+        </View>
+
+        {/* <View>
+          <StackedBarChart
+            data={data}
+            width={SIZES.width}
+            height={220}
+            chartConfig={{
+              backgroundColor: '#1cc910',
+              backgroundGradientFrom: '#eff3ff',
+              backgroundGradientTo: '#efefef',
+              decimalPlaces: 2,
+              color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+              style: {
+                borderRadius: 16
+              }
+            }}
+            style={{
+              marginVertical: 8,
+              borderRadius: 16
+            }}
+          />
+        </View> */}
 
       </ScrollView>
     </SafeAreaView>
