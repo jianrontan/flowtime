6ac66207d7c873b6246482e8c3822f9d68da59af
jianrontan <116509027+jianrontan@users.noreply.github.com>
2023-08-23 19:50:04 +0800

    Changed firebase.ts such that it works with both .env and EAS account wide secrets

--- config/firebase.ts
+++ config/firebase.ts
@@ -2,14 +2,20 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import 'firebase/auth';
-import Constants from "expo-constants";
 
-import firebaseConfig from "../firebaseConfig";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
