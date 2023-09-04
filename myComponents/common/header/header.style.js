15b5ceba8a773ea68bd29fcf0475fc22c0733881
jianrontan <116509027+jianrontan@users.noreply.github.com>
2023-09-04 16:45:02 +0800

    Cleaning up files

---
+++ myComponents/common/header/header.style.js
@@ -0,0 +1,27 @@
+import { StyleSheet } from "react-native";
+
+import { COLORS, FONT, SIZES } from "../../../../constants";
+
+const styles = StyleSheet.create({
+    container: {
+        width: "100%",
+    },
+    headerStyle: {
+        fontFamily: FONT.bold,
+        fontSize: SIZES.large,
+        color: COLORS.themeColor,
+    },
+    backButton: {
+        paddingLeft: 10,
+    },
+    logoutTO: {
+        paddingTop: SIZES.mediumlarge,
+        paddingLeft: SIZES.mediumlarge,
+    },
+    logoutDrawerText:{
+        color: COLORS.themeColor,
+        fontFamily: FONT.medium,
+    },
+})
+
+export default styles;
